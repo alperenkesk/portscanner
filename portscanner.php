9414291b91fb16fcd192e9f4eaa5410ce6d1401a
alperenkesk <alperenkesk55@gmail.com>
2017-03-15 15:26:02 +0200

    Update portscanner.php

--- portscanner.php
+++ portscanner.php
@@ -3,8 +3,10 @@ error_reporting(~E_ALL);
 
 // Port Range // Port Araligi
 $start = 1;
-$finish = 1024;
- 
+$finish = 65535;
+   
+  echo "PORT STATE \n";
+
 $host = '192.168.1.103'; // Target IP // Hedef IP
  
 for($port = $start; $port <= $finish ; $port++)
@@ -12,7 +14,6 @@ for($port = $start; $port <= $finish ; $port++)
   $fsp = fsockopen($host , $port);
   if ($fsp)
   {
-    echo "PORT STATE \n";
     echo "$port   open \n";
     fclose($fsp);
   } 
